1d4af877083517ea0a5a08d72f17d40dc2dd891c
SlyCat <cat.danger@gmail.com>
2018-10-26 13:22:24 +0700

    edit data&time code

--- public/js/lar.js
+++ public/js/lar.js
@@ -123,17 +123,17 @@ jQuery(function($) {
 			var copiedEventObject = $.extend({}, originalEventObject)
 			
             // assign it the date that was reported
-            console.log(date)
             copiedEventObject.start = date
             copiedEventObject.allDay = true
             if (copiedEventObject.end == null && resourceId.type == 'agendaDay') { 
-                copiedEventObject.end = new Date(date._d.getTime()-18000000)
+                copiedEventObject.end = $.fullCalendar.moment(moment(date).add(2, "h"))
+                //copiedEventObject.end._i[3]=copiedEventObject.end._i[3]+2
                 copiedEventObject.allDay = false
             }
 			if($extraEventClass) copiedEventObject['className'] = [$extraEventClass]
             copiedEventObject.id = idGen.getId()
             createEvent = true
-            var newEventStart = date._d.getTime()
+            var newEventStart = copiedEventObject.start._d.getTime()
             var newEventClass = copiedEventObject.className[0]
             var newEvent = ''
             var existingEvents = $("#calendar").fullCalendar("clientEvents", function (evt) {
@@ -144,12 +144,12 @@ jQuery(function($) {
                     var oldEventEnd = evt.end._d.getTime()
                     if (newEventStart >= oldEventStart && oldEventEnd > newEventStart) { newEvent = true }
                     else if (copiedEventObject.end) { 
-                        var newEventEnd = copiedEventObject.end.getTime()
+                        var newEventEnd = copiedEventObject.end._d.getTime()
                         if (newEventStart < oldEventStart && newEventEnd > oldEventStart) { newEvent = true }
                     }
                 }
                 else if (copiedEventObject.end) { 
-                    var newEventEnd = copiedEventObject.end.getTime()
+                    var newEventEnd = copiedEventObject.end._d.getTime()
                     if (newEventEnd > oldEventStart && newEventEnd <= oldEventEnd) { newEvent = true }
                     else if (evt.allDay) {
                         if (oldEventStart < newEventStart && newEventEnd < (oldEventStart + 86399999)) { newEvent = true }
@@ -165,7 +165,11 @@ jQuery(function($) {
                 }
             }
             else if (moment(evt.start._d).isSame(date._d)) { newEvent = true }
-
+            //console.log(new Date(newEventStart))
+            //console.log(new Date(newEventEnd))
+            //console.log(new Date(oldEventStart))
+            //console.log(new Date(oldEventEnd))
+            //console.log(evt.title)
             if (newEvent) {
                 return true
                 } else {
@@ -178,10 +182,16 @@ jQuery(function($) {
             if (resourceId.type == 'agendaDay') { 
                 sdTime = Number(bh.start.split(':')[0]) + Number(bh.start.split(':')[1]/60)
                 edTime = Number(bh.end.split(':')[0]) + Number(bh.end.split(':')[1]/60)
-                seTime = new Date(copiedEventObject.start).getHours()-7 + new Date(copiedEventObject.start).getMinutes()/60
-                eeTime = new Date(copiedEventObject.end).getHours() + new Date(copiedEventObject.end).getMinutes()/60
+                seTime = copiedEventObject.start._i[3] + copiedEventObject.start._i[4]/60
+                eeTime = copiedEventObject.end._i[3] + copiedEventObject.end._i[4]/60
                 if (seTime < sdTime) { createEvent = false }
                 if (eeTime > edTime) { createEvent = false }
+                console.log(copiedEventObject.start)
+                console.log(copiedEventObject.end)
+            console.log(sdTime)
+            console.log(edTime)
+            console.log(seTime)
+            console.log(eeTime)
             }
             else if ($.inArray(date._d.getDay(),bh.dow) < 0) { createEvent = false }
             else if (tddate >= copiedEventObject.start && copiedEventObject.className != 'label-success' && copiedEventObject.className != 'label-grey') { createEvent = false }
@@ -210,15 +220,13 @@ jQuery(function($) {
                     }
                 })
                 if (copiedEventObject.start !== null) {
-                        if (copiedEventObject.start._d !== null) {
-                        data.start = new Date(copiedEventObject.start._d.getTime()-25200000);
-                        }
+                        data.start = copiedEventObject.start
                     }
                     else {
                         data.start = null;
                     }
-                if (copiedEventObject.end !== null) {
-                    data.end = copiedEventObject.end;
+                if (copiedEventObject.end != null) {
+                    data.end = copiedEventObject.end
                 }
                 if (copiedEventObject.className !== null) {
                     data.className = copiedEventObject.className[0]
@@ -261,7 +269,11 @@ jQuery(function($) {
                     if (moment(evt.start._d).isSame(event.start._d)) { newEvent = true }
                     else { newEvent = false }
                 }
-
+                console.log(new Date(newEventStart))
+            console.log(new Date(newEventEnd))
+            console.log(new Date(oldEventStart))
+            console.log(new Date(oldEventEnd))
+            console.log(evt.title)
                 if (newEvent) {
                     return true
                 } else {
@@ -330,7 +342,11 @@ jQuery(function($) {
                 }
             }
             else if (moment(evt.start._d).isSame(event.start._d)) { newEvent = false }            
-
+            //console.log(new Date(newEventStart))
+            //console.log(new Date(newEventEnd))
+            //console.log(new Date(oldEventStart))
+            //console.log(new Date(oldEventEnd))
+            //console.log(evt.title)
             if (newEvent) {
                 return true
             } else {
