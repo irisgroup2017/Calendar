142f976e9df7bf224f09f15ce512430d201bb56a
SlyCat <cat.danger@gmail.com>
2018-11-01 14:46:17 +0700

    update app js

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ require('dotenv').config()
 
 function handleDisconnect() {
 	var con = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '#Iris@2013',
-		database: 'calendar',
+		host: process.env.DB_HOST,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_NAME,
 	})
   
 	con.connect(function(err) { 
