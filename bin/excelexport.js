3db55991ebf394e9a92ea6a4e907ff8408ee2425
SlyCat <cat.danger@gmail.com>
2019-01-10 13:48:29 +0700

    add data export code

--- bin/excelexport.js
+++ bin/excelexport.js
@@ -1,23 +1,31 @@
 const express = require('express'),
 xlsx = require('excel4node'),
 con = require('../bin/mysql'),
-workbook = new xlsx.Workbook()
+workbook = new xlsx.Workbook(),
+moment = require("moment"),
+fs = require('fs')
+momentDurationFormatSetup = require("moment-duration-format")
 
-async function xlCreate(res) {
-    var ws , j=0 , userName , k=1
-    var result = await con.q('SELECT * FROM lar_data ORDER BY userName')
+String.prototype.allReplace = function(obj) {
+    var retStr = this
+    for (var x in obj) {
+        retStr = retStr.replace(new RegExp(x, 'g'), obj[x])
+    }
+    return retStr
+}
+
+async function xlCreate(tstart,tend,res) {
+    var result = await con.q('SELECT * FROM lar_data WHERE start >= ? AND start <= ? OR end >= ? AND end <= ? ORDER BY userName ASC , start ASC , end ASC',[tstart,tend,tstart,tend]),
+    ws = workbook.addWorksheet('report') , userName , k=1
+    ws.cell(k,1).string('ชื่อ').style({alignment:{horizontal:'center'}})
+    ws.cell(k,2).string('วันที่ลา').style({alignment:{horizontal:'center'}})
+    ws.cell(k,3).string('ประเภทลา').style({alignment:{horizontal:'center'}})
+    ws.cell(k,4).string('รายละเอียดการลา').style({alignment:{horizontal:'center'}})
+    ws.cell(k,5).string('จำนวนวันลา').style({alignment:{horizontal:'center'}})
+    k++
     for (i = 0;i<result.length;i++) {
         if (userName != result[i].userName) {
-            ws = workbook.addWorksheet(result[i].userName)
-            j = j+1
-            k = 1
-            ws.cell(k,1).string('test1').style({alignment:{horizontal:'center'}})
-            ws.cell(k,2).string('test2').style({alignment:{horizontal:'center'}})
-            ws.cell(k,3).string('test3').style({alignment:{horizontal:'center'}})
-            ws.cell(k,4).string('test4').style({alignment:{horizontal:'center'}})
-            k++
-        } else {
-            k++
+            ws.cell(k,1).string(result[i].userName)
         }
         userName = result[i].userName
         if (result[i].className == 'label-grey') { larType = 'ลาป่วย' }
@@ -26,13 +34,40 @@ async function xlCreate(res) {
         else { larType = result[i].title }
         title = result[i].title
         start = new Date(result[i].start*1000)
-        end = (result[i].end ? new Date((result[i].end-60)*1000) : '-' )
-        ws.cell(k,1).string(larType)
-        ws.cell(k,2).string(title)
-        ws.cell(k,3).date(start).style({numberFormat: 'dd/mm/yyyy',alignment:{horizontal:'center'}})
-        if (end == '-') { ws.cell(k,4).string(end).style({alignment:{horizontal:'center'}}) } else { ws.cell(k,4).date(end).style({numberFormat: 'dd/mm/yyyy',alignment:{horizontal:'center'}}) }
+        end = (result[i].end ? new Date((result[i].end)*1000) : '-' )
+        ws.cell(k,2).date(start).style({numberFormat: 'dd/mm/yyyy',alignment:{horizontal:'center'}})
+        ws.cell(k,3).string(larType)
+        ws.cell(k,4).string(title)
+        if (end == '-') { 
+            ws.cell(k,5).string('1 วัน').style({alignment:{horizontal:'center'}}) } 
+        else {
+            //1 ชม 3600 , 1 วัน 86400
+            duration = (result[i].end > tend ? tend : result[i].end) - (result[i].start < tstart ? tstart : result[i].start)
+            duration = moment.duration(duration,'second').format("d,[d],h,[h],m,[m]")
+            duration = duration.split(',')
+            duration = durt(duration)
+            ws.cell(k,5).string(duration).style({alignment:{horizontal:'center'}}) 
+        }
+        k++
     }
+    //if (fs.existsSync('excelexport.xlsx')) { fs.unlinkSync('excelexport.xlsx') }
+    console.log('test')
     workbook.write('excelexport.xlsx',res)
 }
-
+function durt(obj) {
+    var k = 0, ans=''
+    for (var i in obj) {
+        x = obj[i]
+        if (k == 0) {
+            if (x > 0) {
+                ans = ans +''+ x + ' '
+                k = 1
+            }
+        } else {
+            ans = ans +''+ x + ' '
+            k = 0
+        }
+    }
+    return ans.allReplace({'d':'วัน','h':'ชั่วโมง','m':'นาที'})
+}
 exports.xlCreate = xlCreate
--- routes/excelexport.js
+++ routes/excelexport.js
@@ -4,8 +4,32 @@ excel = require('../bin/excelexport'),
 log = require('../bin/logger')
 
 router.get('/', async function(req, res) {
-    excel.xlCreate(res)
+    if (dataop < 3) { res.redirect('/') }
+    var userName = req.cookies.user_name,dataid = req.cookies.user_dataid,dataop = req.cookies.user_op,mail = req.cookies.user_mail
+	if (userName) {
+		data = {
+			'username': userName,
+			'dataid': dataid,
+			'operator': dataop,
+			'mail': mail
+		}
+		parms = { title: 'นำออกข้อมูล', head1: 'Excel Export' }
+		parms.user = userName
+		parms.operator = dataop
+	} else {
+		res.redirect('/login')
+    }
+    res.render('excelexport', parms)
+})
+
+router.get('/export', async function(req, res) {
+    excel.xlCreate(req.query.datestart,req.query.dateend,res)
     log.logger('info','Excel Exported: '+ req.cookies.user_name)
+})
+
+router.post('/',async function(req,res) {
+	rlink = '/excelexport/export?datestart='+req.body.datea+'&dateend='+req.body.dateb
+	res.json(rlink)
 })
 
 module.exports = router
