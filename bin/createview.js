2b19ae10c4842c6415e4ee5ae833b5ebc0709520
ITIRIS <it@iris.co.th>
2019-02-25 10:08:45 +0700

    update

--- bin/createview.js
+++ bin/createview.js
@@ -15,7 +15,7 @@ function ctvcy() {
     var name,nameas
     for (var i=0;i<dpn.length;i++) {
         for (var j=0;j<9;j++) {
-            name = dpns[i] + ll[j] + '2019' 
+            name = 'tv_' + dpns[i] + ll[j] + '2019' 
             nameas = dpn[i] + ll[j] + '2019'
             if (j <= 3) {
                 con.q('CREATE OR REPLACE VIEW '+name+' AS SELECT count(lar_data.dataid) AS '+nameas+' FROM (user_data left join lar_data on((lar_data.dataid = user_data.dataid))) WHERE ((user_data.depart = "'+depart[i]+'") and (lar_data.start >= 1546300800) and (lar_data.start < 1577836800) and (lar_data.className = "'+cl[j]+'"))')
@@ -38,7 +38,7 @@ function ctvcm() {
         for (var j=0;j<12;j++) {
             month = new Date(year,j,1).getTime()+25200000
             nmonth = new Date(2019,j+1,1).getTime()+25200000
-            name = dpns[i] + (new Date(month).getMonth()+1) + '2019'
+            name = 'tv_' + dpns[i] + (new Date(month).getMonth()+1) + '2019'
             nameas = dpn[i] + (new Date(nmonth).getMonth()+1) + '2019'
             con.q('CREATE OR REPLACE VIEW '+name+' AS SELECT count(lar_data.dataid) AS '+nameas+' FROM (user_data left join lar_data on((lar_data.dataid = user_data.dataid))) WHERE ((user_data.depart = "'+depart[i]+'") and (lar_data.start >= '+month/1000+') and (lar_data.start < '+nmonth/1000+'))')
         }
