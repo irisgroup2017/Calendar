cbd4243a221b2f2a82230cb2afaef41f537ae7b2
RighteouskilL <38749326+RighteouskilL@users.noreply.github.com>
2020-10-19 11:43:59 +0700

    update

--- routes/reporttime.js
+++ routes/reporttime.js
@@ -14,8 +14,16 @@ router.get('/', async function(req, res, next) {
   let table = "em" + dataid.toString()
   let tableexist = await con.q('SHOW TABLES FROM calendar LIKE ?',[table])
   if (tableexist) {
-   let larstart = moment(timeStart,"YYYY-MM-DD").add(7,'hours').unix()
-   let larend = moment(timeEnd,"YYYY-MM-DD").add(7,'hours').unix()
+   let larstart = dateconvert.add7unixs(timeStart)
+   let larend = dateconvert.add7unixs(timeEnd)
+   let vacationstart = dateconvert.add7unixms(timeStart)
+   let vacationend = dateconvert.add7unixms(timeEnd)
+   let privacy = await con.q('SELECT wplace,swtime,ewtime,cdate FROM privacy_data WHERE dataid = ?',[dataid])
+   let wplace = privacy[0].wplace
+   let vacationquery = 'SELECT dtitle,'+(wplace == 1 ? "doffice" : "dsite")+' AS time FROM vacation_list WHERE '+(wplace == 1 ? "doffice" : "dsite")+' BETWEEN ? AND ?'
+   let vacationlist = await con.q(vacationquery,[vacationstart,vacationend])
+   vacationlist = vacationlist.reduce((acc,it) => (acc[dateconvert.changeformat(it.time/1000)] = it,acc),{})
+   console.log(vacationlist)
    let larlist = await con.q('SELECT d.title,d.className,t.lartype,d.start,d.end,d.swapDate,d.allDay FROM lar_data AS d JOIN lar_type AS t ON d.className = t.classname WHERE dataid = ? AND ((d.start BETWEEN ? AND ?) OR (d.end BETWEEN ? AND ?))',[dataid,larstart,larend,larstart,larend])
    let datelist = datetodate(timeStart,timeEnd)
    let result = (await con.q('SELECT emid,depart FROM user_data WHERE dataid = ?',[dataid]))[0]
@@ -27,23 +35,22 @@ router.get('/', async function(req, res, next) {
     mail: mail,
     emid: result.emid,
     depart: result.depart,
-    datestart: moment(timeStart,"YYYY-MM-DD").locale("th").format('DD MMMM YYYY'),
-    dateend: moment(timeEnd,"YYYY-MM-DD").locale("th").format('DD MMMM YYYY'),
+    datestart: dateconvert.thformat(timeStart),
+    dateend: dateconvert.thformat(timeEnd),
     datelist: datetodate(timeStart,timeEnd),
-    dateshow: datelist.map(x => moment(x,'YYYY-MM-DD').locale("th").format('DD MMM YYYY'))
+    dateshow: datelist.map(x => dateconvert.thformat(x))
    }
    query = 'SELECT DATE_FORMAT(u.date,"%Y-%m-%d") AS date,u.timestart,u.timeend,ms.MachShort AS mstart,me.MachShort AS mend FROM '+table+' AS u JOIN machine_data AS ms on u.MachCodeStart = ms.MachCode JOIN machine_data AS me on u.MachCodeEnd = me.MachCode WHERE (date BETWEEN ? AND ?)'
    result = await con.q(query,[timeStart,timeEnd])
    result = result.reduce((acc,it) => (acc[it.date] = it,acc),{})
-   //moment(it.date,"YYYY-MM-DD").format('DD-MMM-YYYY')
    parms.fingerscan = result
+   parms.vacation = vacationlist
    console.log(larlist)
    parms.lartype = larlist.reduce((acc,it) => (
-    acc[moment.unix(it.start).format('YYYY-MM-DD')] = {
-     lartype: (it.lartype == "ลาอื่นๆ" ? it.title : it.lartype) +""+(it.lartype == "ลาสลับวันหยุด" ? "กับวันที่ "+ moment.unix(it.swapDate).locale("th").format('DD MMM YYYY')+" ": "") +""+ (it.allDay ? "ทั้งวัน" : " "+moment.duration((it.end-it.start)*1000).asHours()+" ชั่วโมง")
+    acc[dateconvert.changeformat(it.start)] = {
+     lartype: (it.lartype == "ลาอื่นๆ" ? it.title : it.lartype) +""+(it.lartype == "ลาสลับวันหยุด" ? "กับวันที่ "+ dateconvert.thformat(it.swapDate)+" ": "") +""+ (it.allDay ? "ทั้งวัน" : " "+dateconvert.durationhours((it.end-it.start)*1000)+" ชั่วโมง")
     }
     ,acc),{})
-   //console.log(parms)
    res.render('reporttime', parms)
   }
  } else { res.redirect('/') }
@@ -60,4 +67,22 @@ function datetodate(datestart,dateend) {
  }
  result.push(date)
  return result
+}
+
+let dateconvert = {
+ add7unixs: function(date) {
+  return moment(date,"YYYY-MM-DD").add(7,'hours').unix()
+ },
+ add7unixms: function(date) {
+  return moment(date,"YYYY-MM-DD").add(7,'hours').valueOf()
+ },
+ thformat: function(date) {
+  return moment(date,"YYYY-MM-DD").locale("th").format('DD MMMM YYYY')
+ },
+ changeformat: function(date) {
+  return moment.unix(date).format('YYYY-MM-DD')
+ },
+ durationhours: function(date) {
+  return moment.duration(date).asHours()
+ }
 }
