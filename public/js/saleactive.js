52fb7cd4cf1144b498902bc751fb198272ebad66
RighteouskilL <38749326+RighteouskilL@users.noreply.github.com>
2020-11-26 11:31:55 +0700

    add saleactive page
    + show place where sale be stationed

---
+++ public/js/saleactive.js
@@ -0,0 +1,114 @@
+jQuery(function ($) {
+ let date = new Date()
+ var calendar = $('#calendar').fullCalendar({
+  buttonHtml: {
+   prev: '<i class="ace-icon fa fa-chevron-left"></i>',
+   next: '<i class="ace-icon fa fa-chevron-right"></i>'
+  },
+  customButtons: {
+   year: {
+    text: 'ปี',
+    click: function () {
+     var cvy = parseInt($('.fc-center h2').text().split(' ')[1])
+     calendar.fullCalendar('changeView', 'basic', {
+      start: new Date(cvy, 0, 1, 7),
+      end: new Date(cvy, 11, 32, 7)
+     })
+     $('.fc-prev-button').attr('disabled', 'disabled')
+     $('.fc-next-button').attr('disabled', 'disabled')
+    }
+   }
+  },
+  header: {
+   left: 'prev,next today',
+   center: 'title',
+   right: 'year,month,agendaDay'
+  },
+  longPressDelay: 1000,
+  nowIndicator: true, // place timeline in day mode
+  now: date, // assign time to calendar
+  navLinks: true, // allows navigate to day mode when click number of day
+  displayEventEnd: true,
+  timeFormat: 'H:mm',
+  selectable: true,
+  eventLimit: true,
+
+  dayRender: function (date, el, view) {
+   var thisdate = []
+   if (sessionStorage.date) {
+    thisdate = JSON.parse(sessionStorage.date)
+    thisdate.push(date._d.getTime())
+    thisdate = JSON.stringify(thisdate)
+   } else {
+    thisdate.push(date._d.getTime())
+    thisdate = JSON.stringify(thisdate)
+   }
+   sessionStorage.setItem('date', thisdate)
+  },
+
+  viewRender: function (view, element) {
+   if (view.type == "month" || view.type == "agendaDay" || view.type == "basic") {
+    $('#calendar').fullCalendar('removeEvents', function (e) {
+     return !e.isUserCreated
+    })
+    let start = view.start._i / 1000
+    let end = view.end._i / 1000
+    $.ajax({
+     url: '/saleactive/load',
+     type: "POST",
+     async: false,
+     data: {
+      start: start,
+      end: end
+     },
+     success: function (data) {
+      console.log(data)
+      $.each(data, function (i, item) {
+       $('#calendar').fullCalendar('renderEvent', item)
+      })
+     }
+    })
+   }
+  },
+  
+  eventAfterAllRender: function (view) {
+   var listday = JSON.parse(sessionStorage.date)
+   if (view.type == 'basic') {
+    for (var i = 0; i < listday.length; i++) {
+     thisday = new Date(listday[i])
+     if (thisday.getFullYear() != view.title) {
+      datewrite = thisday.getFullYear() + '-' + ("0" + (thisday.getMonth() + 1)).slice(-2) + '-' + ("0" + thisday.getDate()).slice(-2)
+      $('.fc-day-top[data-date="' + datewrite + '"]').addClass('fc-other-month')
+     }
+    }
+  
+    $.ajax({
+     url: '/lar',
+     type: "POST",
+     dataType: "json",
+     async: false,
+     data: {
+      'state': 'getvacation',
+      'start': view.start._i,
+      'end': view.end._i
+     },
+     success: async function (data) {
+      var len = data.mydata.length
+      for (var i = 0; i < len; i++) {
+       if (data.mydata[i]) {
+        thisvacation = data.mydata[i][data.wplace]
+        if (listday.indexOf(thisvacation)) {
+         datewrite = new Date(thisvacation).getFullYear() + '-' + ("0" + (new Date(thisvacation).getMonth() + 1)).slice(-2) + '-' + ("0" + new Date(thisvacation).getDate()).slice(-2)
+         $('.fc-bg td[data-date="' + datewrite + '"').append('<div class="vdate">' + data.mydata[i].dtitle + '</div>')
+        }
+       }
+      }
+     }
+    })
+    //sessionStorage.removeItem('date')
+    //sessionStorage.removeItem('fingerscan')
+   }
+  },
+
+ })
+})
--- routes/saleactive.js
+++ routes/saleactive.js
@@ -1,2 +1,52 @@
+const express = require('express')
+const router = express.Router()
 const con = require('../bin/mysql')
 const moment = require('moment')
+const site = ['','','','','','','label-warning','','label-success','','','label-danger','label-dark','label-primary']
+
+router.get('/', async function(req, res) {
+ parms = {
+  user: req.cookies.user_name,
+  operator: req.cookies.user_op,
+  title: "แสดงข้อมูลเซลล์ประจำโครงการ"
+ }
+ res.render('saleactive',parms)
+})
+
+router.post('/load',async function(req,res) {
+ let start = moment.unix(req.body.start).format("YYYY-MM-DD")
+ let end = moment.unix(req.body.end).format("YYYY-MM-DD")
+ let query = "SELECT cd.dataid,cd.name FROM contact_data cd LEFT JOIN user_data ud ON ud.dataid = cd.dataid WHERE cd.level = 11 AND ud.status = 1"
+ let result = await con.q(query)
+ let dataFinger = result.map(id => ("em"+id.dataid).toString(),[])
+ let dataPerson = result.map(id => id.name,[])
+ let objs = []
+ let rcount = 0
+ for (tableid of dataFinger) {
+  query = 'SELECT DATE_FORMAT(date,"%Y-%m-%d") AS date,timestart,timeend,MachCodeStart AS mstart,MachCodeEnd AS mend FROM '+tableid+' AS u WHERE MachCodeStart <> 5 AND MachCodeEnd <> 5 AND (date BETWEEN ? AND ?)'
+  result = (await con.q(query,[start,end])).map(person => {
+   let day = moment(person.date,'YYYY-MM-DD')
+   let stime = person.timestart.split(':')
+   let etime = person.timeend.split(':')
+   if (person.mstart && person.mend) {
+    console.log(person.mstart,person.end)
+   }
+   return {
+    day: person.date,
+    date: moment(person.date,'YYYY-MM-DD').add(7,'hours').valueOf(),
+    start: moment(day).set({ hour: stime[0], minute: stime[1] }).add(7,'hours').valueOf(),
+    end: moment(day).set({ hour: etime[0], minute: etime[1] }).add(7,'hours').valueOf(),
+    title: dataPerson[rcount],
+    user: dataPerson[rcount],
+    className: site[person.mstart] || site[person.mend]
+   }
+  },[])
+  objs.push(result)
+  rcount++
+ }
+ objs = objs.reduce((prev, cur) => prev.concat(cur), [])
+ res.json(objs)
+ //title:text start:unixtimestamp allday:boolean
+})
+
+module.exports = router
