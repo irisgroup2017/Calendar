5e1f110fa1ea40ba6f4a5405ca4fc98d951ce9d0
RighteouskilL <38749326+RighteouskilL@users.noreply.github.com>
2020-11-27 10:44:09 +0700

    add department leave view
    + page for view leave information & date from employee department
    + add menu layout for sale active place and department leave

---
+++ routes/departmentleaveview.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const con = require('../bin/mysql')
+const moment = require('moment')
+const site = ['','','','','','','label-warning','','label-success','','','label-danger','label-dark','label-primary']
+
+router.get('/', async function(req, res) {
+ parms = {
+  user: req.cookies.user_name,
+  operator: req.cookies.user_op,
+  title: "แสดงข้อมูลเซลล์ประจำโครงการ"
+ }
+ res.render('departmentleaveview',parms)
+})
+
+router.post('/load',async function(req,res) {
+ let dataid = req.cookies.user_dataid
+ let start = req.body.start
+ let end = req.body.end
+ let query = "SELECT dataid FROM user_data WHERE status = 1 AND depart = (SELECT depart FROM user_data WHERE dataid = ?)"
+ let result = (await con.q(query,[dataid])).map(it => it.dataid)
+ let objs = []
+ for (id of result) {
+  query = 'SELECT dataid,title,start,end,swapDate,allDay,className,userName,approve FROM lar_data WHERE dataid = ? AND approve > 1 AND (start BETWEEN ? AND ?)'
+  result = (await con.q(query,[id,start,end])).map(item => {
+   let stime = moment.unix(item.start).valueOf()
+   let etime = (item.end ? moment.unix(item.end).valueOf() : "")
+   return {
+    date: stime,
+    start: stime,
+    end: etime,
+    title: item.userName,
+    user: item.userName,
+    allDay: (item.allDay ? true : false),
+    className: (item.approve == 2 ? "" : item.className),
+    description: item.title,
+   }
+  },[])
+  objs.push(result)
+ }
+ objs = objs.reduce((prev, cur) => prev.concat(cur), [])
+ res.json(objs)
+})
+
+module.exports = router
--- routes/leavelist.js
+++ routes/leavelist.js
@@ -1,165 +1,189 @@
 const express = require('express'),
-router = express.Router(),
-con = require('../bin/mysql'),
-ll = require('../bin/larlist'),
-fs = require('fs')
+ router = express.Router(),
+ con = require('../bin/mysql'),
+ ll = require('../bin/larlist'),
+ fs = require('fs')
 var now = new Date(),
-sTime = new Date(now.getFullYear(),0,1,7).getTime()/1000,
-eTime = new Date(now.getFullYear(),11,31,7).getTime()/1000,
-mailsend = require('../bin/mailsend'),
-log = require('../bin/logger')
+ sTime = new Date(now.getFullYear(), 0, 1, 7).getTime() / 1000,
+ eTime = new Date(now.getFullYear(), 11, 31, 7).getTime() / 1000,
+ mailsend = require('../bin/mailsend'),
+ log = require('../bin/logger')
 
-router.get('/', async function(req, res) {
-	var userName = req.cookies.user_name,dataid = req.cookies.user_dataid,dataop = req.cookies.user_op,mail = req.cookies.user_mail
-	parms = { title: 'รายงานการลา', head1: "Detail List" }
-	if (userName) {
-		data = {
-			'username': userName,
-			'dataid': dataid,
-			'operator': dataop,
-			'mail': mail
-		}
-        parms.user = userName
-        parms.operator = dataop
-	} else {
-		res.redirect('/login')
-    }
-    result = await con.q('SELECT * FROM lar_data WHERE dataid = ? AND start BETWEEN ? AND ?',[dataid,sTime,eTime])
-	result2 = await con.q('SELECT * FROM privacy_data WHERE userName = ?',userName)
-	parms.objs = []
-	var end = '',
-	allDay = '',
-	larstatus = '',
-	approvedate = '',
-	dayPlus = 0,
-	hourPlus = 0,
- minPlus = 0
-	for (var i = 0; i < result.length; i++) {
-			if (result[i].end) { end = (result[i].end-25200)*1000 } else { end = null }
-            if (result[i].allDay) {	allDay = true } else { allDay = false }
+router.get('/', async function (req, res) {
+ var userName = req.cookies.user_name,
+  dataid = req.cookies.user_dataid,
+  dataop = req.cookies.user_op,
+  mail = req.cookies.user_mail
+ parms = {
+  title: 'รายงานการลา',
+  head1: "Detail List"
+ }
+ if (userName) {
+  data = {
+   'username': userName,
+   'dataid': dataid,
+   'operator': dataop,
+   'mail': mail
+  }
+  parms.user = userName
+  parms.operator = dataop
+ } else {
+  res.redirect('/login')
+ }
+ result = await con.q('SELECT * FROM lar_data WHERE dataid = ? AND start BETWEEN ? AND ?', [dataid, sTime, eTime])
+ result2 = await con.q('SELECT * FROM privacy_data WHERE userName = ?', userName)
+ parms.objs = []
+ var end = '',
+  allDay = '',
+  larstatus = '',
+  approvedate = '',
+  dayPlus = 0,
+  hourPlus = 0,
+  minPlus = 0
+ for (var i = 0; i < result.length; i++) {
+  if (result[i].end) {
+   end = (result[i].end - 25200) * 1000
+  } else {
+   end = null
+  }
+  if (result[i].allDay) {
+   allDay = true
+  } else {
+   allDay = false
+  }
 
-            if (result[i].delreq == 1) {
-				t = ll.getDateValue(result[i].deldate*1000 + (7 * 60 * 60))
-                approvedate = t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-                approver = result[i].userName
-			}
-            else if (result[i].approve == 1 && result[i].hrapprovedate) {
-				t = ll.getDateValue(result[i].hrapprovedate*1000 + (7 * 60 * 60))
-                approvedate = t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-                approver = result[i].hrapprover
-            }
-            else if (result[i].approve == 2) {
-                approvedate = ""
-                approver = ""
-            }
-			else if (result[i].approve == 3) {
-				t = ll.getDateValue(result[i].approvedate*1000 + (7 * 60 * 60))
-                approvedate = t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-                approver = result[i].approver
-			}
-			else if (result[i].approve == 4) {
-				t = ll.getDateValue(result[i].hrapprovedate*1000 + (7 * 60 * 60))
-                approvedate = t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-                approver = (result[i].hrapprover === "ธนกร จริยเพียรพันธุ์" ? "Admin" : result[i].hrapprover)
-            }
-            else if (result[i].approve == 1) {
-				t = ll.getDateValue(result[i].approvedate*1000 + (7 * 60 * 60))
-                approvedate = t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-                approver = result[i].approver
-            }
-            else if (result[i].approve == 0) {
-				t = ll.getDateValue(result[i].deldate*1000 + (7 * 60 * 60))
-                approvedate = t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-                approver = (result[i].hrapprover === "ธนกร จริยเพียรพันธุ์" ? "Admin" : result[i].hrapprover)
-            }
-            if (result[i].delreq == 1) { larstatus = 'แจ้งยกเลิก: ' + approvedate , classn = 'bg-warning' }
-			else if (result[i].approve == 0) { larstatus = 'ยกเลิก: ' + approvedate , classn = 'strikeout' } 
-			else if (result[i].approve == 1) { larstatus = 'ไม่อนุมัติ: ' + approvedate , classn = 'bg-danger' }
-			else if (result[i].approve == 2) { larstatus = 'รออนุมัติ' , classn = '' }
-			else if (result[i].approve == 3) { larstatus = 'อนุมัติ: ' + approvedate , classn = 'bg-success' }
-            else if (result[i].approve == 4) { larstatus = 'HR อนุมัติ: ' + approvedate , classn = 'bg-info' }
-            
-			if (result[i].className == 'label-grey') { larType = 'ลาป่วย' }
-			else if (result[i].className == 'label-success') { larType = 'ลากิจ' }
-   else if (result[i].className == 'label-warning') { larType = 'ลาพักร้อน' } 
-   else if (result[i].className == 'label-danger') { 
-    larType = 'สลับวันหยุด' 
-    let swaptitle = result[i].title
-    if (swaptitle.match(/\d\d:\d\d:\d\d:\d\d/)) {
-     let swaptime = swaptitle.match(/\d\d/g)
-     let swaptop = (swaptime[0]*40)+((swaptime[1]/30)*20)
-     let swapbottom = (-(swaptime[2]*40)+((swaptime[3]/30)*20))
-     swaptime = swaptitle.match(/\d\d:\d\d/g)
-     let swapre = swaptime[0] +":"+ swaptime[1]
-     swaptime = swaptime[0] +"-"+ swaptime[1]
-     result[i].title = swaptitle.replace(swapre,"")
-    }
-   } 
-			else { larType = result[i].title }
-			timeKeep = ll.getDayTime(result[i].start,end,allDay,result2[0].swtime.substring(0,5),result2[0].ewtime.substring(0,5))
-			t = ll.getDateValue(result[i].cTime)
-			cTime = t.dy+', '+t.da+' '+t.mo+' '+t.y+' ('+t.h+':'+t.mi+')'
-			parms.objs.push({
-    id: result[i].id,
-				lartype: larType,
-				title: result[i].title,
-				dateStart: timeKeep.dateStart,
-				dateEnd: timeKeep.dateEnd,
-				timeStart: timeKeep.timeStart,
-				timeEnd: timeKeep.timeEnd,
-				daylength: timeKeep.daylength,
-				cTime: cTime,
-				larstatus: larstatus,
-				approver: approver,
-				aclass: classn
-			})
-			tokenDate = timeKeep.daylength.split(' ')
-			if (tokenDate[1] === 'วัน') { dayPlus = parseInt(tokenDate[0]) + dayPlus } 
-			else { 
-				hourPlus = parseInt(tokenDate[0]) + hourPlus 
-				if (tokenDate[2]) { minPlus = parseInt(tokenDate[2]) }
-			}
-			if (minPlus > 60) { 
-				hourPlus = hourPlus+ (minPlus / 60)
-				minPlus = minPlus % 60
-			}
-			if (hourPlus > 8) {
-				dayPlus = dayPlus + (hourPlus / 8)
-				hourPlus = hourPlus % 8
-			}
-			parms.lengthPlus = dayPlus +' วัน ' + hourPlus + ' ชั่วโมง '+ (minPlus > 0 ? minPlus +' นาที' : "")
-		}
-    parms.tbl = parms.objs.length
-    res.render('leavelist', parms)
+  if (result[i].delreq == 1) {
+   t = ll.getDateValue(result[i].deldate * 1000 + (7 * 60 * 60))
+   approvedate = t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+   approver = result[i].userName
+  } else if (result[i].approve == 1 && result[i].hrapprovedate) {
+   t = ll.getDateValue(result[i].hrapprovedate * 1000 + (7 * 60 * 60))
+   approvedate = t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+   approver = result[i].hrapprover
+  } else if (result[i].approve == 2) {
+   approvedate = ""
+   approver = ""
+  } else if (result[i].approve == 3) {
+   t = ll.getDateValue(result[i].approvedate * 1000 + (7 * 60 * 60))
+   approvedate = t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+   approver = result[i].approver
+  } else if (result[i].approve == 4) {
+   t = ll.getDateValue(result[i].hrapprovedate * 1000 + (7 * 60 * 60))
+   approvedate = t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+   approver = (result[i].hrapprover === "ธนกร จริยเพียรพันธุ์" ? "Admin" : result[i].hrapprover)
+  } else if (result[i].approve == 1) {
+   t = ll.getDateValue(result[i].approvedate * 1000 + (7 * 60 * 60))
+   approvedate = t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+   approver = result[i].approver
+  } else if (result[i].approve == 0) {
+   t = ll.getDateValue(result[i].deldate * 1000 + (7 * 60 * 60))
+   approvedate = t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+   approver = (result[i].hrapprover === "ธนกร จริยเพียรพันธุ์" ? "Admin" : result[i].hrapprover)
+  }
+  if (result[i].delreq == 1) {
+   larstatus = 'แจ้งยกเลิก: ' + approvedate, classn = 'bg-warning'
+  } else if (result[i].approve == 0) {
+   larstatus = 'ยกเลิก: ' + approvedate, classn = 'strikeout'
+  } else if (result[i].approve == 1) {
+   larstatus = 'ไม่อนุมัติ: ' + approvedate, classn = 'bg-danger'
+  } else if (result[i].approve == 2) {
+   larstatus = 'รออนุมัติ', classn = ''
+  } else if (result[i].approve == 3) {
+   larstatus = 'อนุมัติ: ' + approvedate, classn = 'bg-success'
+  } else if (result[i].approve == 4) {
+   larstatus = 'HR อนุมัติ: ' + approvedate, classn = 'bg-info'
+  }
+
+  if (result[i].className == 'label-grey') {
+   larType = 'ลาป่วย'
+  } else if (result[i].className == 'label-success') {
+   larType = 'ลากิจ'
+  } else if (result[i].className == 'label-dark') {
+   larType = 'ลากิจไม่รับค่าจ้าง'
+  } else if (result[i].className == 'label-warning') {
+   larType = 'ลาพักร้อน'
+  } else if (result[i].className == 'label-danger') {
+   larType = 'สลับวันหยุด'
+   let swaptitle = result[i].title
+   if (swaptitle.match(/\d\d:\d\d:\d\d:\d\d/)) {
+    let swaptime = swaptitle.match(/\d\d/g)
+    let swaptop = (swaptime[0] * 40) + ((swaptime[1] / 30) * 20)
+    let swapbottom = (-(swaptime[2] * 40) + ((swaptime[3] / 30) * 20))
+    swaptime = swaptitle.match(/\d\d:\d\d/g)
+    let swapre = swaptime[0] + ":" + swaptime[1]
+    swaptime = swaptime[0] + "-" + swaptime[1]
+    result[i].title = swaptitle.replace(swapre, "")
+   }
+  } else {
+   larType = result[i].title
+  }
+  timeKeep = ll.getDayTime(result[i].start, end, allDay, result2[0].swtime.substring(0, 5), result2[0].ewtime.substring(0, 5))
+  t = ll.getDateValue(result[i].cTime)
+  cTime = t.dy + ', ' + t.da + ' ' + t.mo + ' ' + t.y + ' (' + t.h + ':' + t.mi + ')'
+  parms.objs.push({
+   id: result[i].id,
+   lartype: larType,
+   title: result[i].title,
+   dateStart: timeKeep.dateStart,
+   dateEnd: timeKeep.dateEnd,
+   timeStart: timeKeep.timeStart,
+   timeEnd: timeKeep.timeEnd,
+   daylength: timeKeep.daylength,
+   cTime: cTime,
+   larstatus: larstatus,
+   approver: approver,
+   aclass: classn
+  })
+  tokenDate = timeKeep.daylength.split(' ')
+  if (tokenDate[1] === 'วัน') {
+   dayPlus = parseInt(tokenDate[0]) + dayPlus
+  } else {
+   hourPlus = parseInt(tokenDate[0]) + hourPlus
+   if (tokenDate[2]) {
+    minPlus = parseInt(tokenDate[2])
+   }
+  }
+  if (minPlus > 60) {
+   hourPlus = hourPlus + (minPlus / 60)
+   minPlus = minPlus % 60
+  }
+  if (hourPlus > 8) {
+   dayPlus = dayPlus + (hourPlus / 8)
+   hourPlus = hourPlus % 8
+  }
+  parms.lengthPlus = dayPlus + ' วัน ' + hourPlus + ' ชั่วโมง ' + (minPlus > 0 ? minPlus + ' นาที' : "")
+ }
+ parms.tbl = parms.objs.length
+ res.render('leavelist', parms)
 })
 
-router.post('/',async function(req,res) {
+router.post('/', async function (req, res) {
  if (req.body.state == 'delete') {
-		var filename = await con.q('SELECT fname FROM lar_data WHERE id = ?',req.body.larid)
-		if (filename[0].fname != '') { 
-			var path = __basedir + '/bin/doc/' + req.cookies.user_name + '/' +filename[0].fname 
+  var filename = await con.q('SELECT fname FROM lar_data WHERE id = ?', req.body.larid)
+  if (filename[0].fname != '') {
+   var path = __basedir + '/bin/doc/' + req.cookies.user_name + '/' + filename[0].fname
    if (fs.existsSync(path)) {
     fs.unlinkSync(path)
    }
-		}
-		await con.q('DELETE FROM lar_data WHERE id = ?',req.body.larid)
-		log.logger('info','Delete Leave Data : '+ req.cookies.user_name +' ID '+ req.body.larid)
-	}
-	if (req.body.state == 'delkeep') {
-		await con.q('UPDATE lar_data SET approve = ?,deldate = ? WHERE id = ?',[0,now/1000,req.body.larid])
-		mailsend.send('ทำการยกเลิกการ',req.cookies.user_name,req.body.larid,'boss')
-		log.logger('info','Send delete request to BOSS: '+ req.cookies.user_name +' Leave ID '+ req.body.larid)
+  }
+  await con.q('DELETE FROM lar_data WHERE id = ?', req.body.larid)
+  log.logger('info', 'Delete Leave Data : ' + req.cookies.user_name + ' ID ' + req.body.larid)
+ }
+ if (req.body.state == 'delkeep') {
+  await con.q('UPDATE lar_data SET approve = ?,deldate = ? WHERE id = ?', [0, now / 1000, req.body.larid])
+  mailsend.send('ทำการยกเลิกการ', req.cookies.user_name, req.body.larid, 'boss')
+  log.logger('info', 'Send delete request to BOSS: ' + req.cookies.user_name + ' Leave ID ' + req.body.larid)
  }
  if (req.body.state == 'hrprove') {
-		await con.q('UPDATE lar_data SET delreq = ?,deldate = ? WHERE id = ?',[1,now/1000,req.body.larid])
-		mailsend.send('แจ้งขอยกเลิกการ',req.cookies.user_name,req.body.larid,'hr')
-		log.logger('info','Send delete request to HR: '+ req.cookies.user_name +' Leave ID '+ req.body.larid)
+  await con.q('UPDATE lar_data SET delreq = ?,deldate = ? WHERE id = ?', [1, now / 1000, req.body.larid])
+  mailsend.send('แจ้งขอยกเลิกการ', req.cookies.user_name, req.body.larid, 'hr')
+  log.logger('info', 'Send delete request to HR: ' + req.cookies.user_name + ' Leave ID ' + req.body.larid)
  }
  res.json(req.body)
 })
 
-router.post('/resend',async function(req,res,next) {
- mailsend.send('Resend: ขออนุญาติ',req.cookies.user_name,req.body.larid,'boss')
+router.post('/resend', async function (req, res, next) {
+ mailsend.send('Resend: ขออนุญาติ', req.cookies.user_name, req.body.larid, 'boss')
  res.end('ok')
 })
 
