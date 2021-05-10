5dcc932bf7ed4f560aa4f4a1ed6d8e6227b59b9d
RighteouskilL <it@iris.co.th>
2021-05-10 11:15:13 +0700

    Update scriptscore.js

--- routes/scriptscore.js
+++ routes/scriptscore.js
@@ -88,8 +88,7 @@ module.exports = {
     }
     else {
      let users = obj.split(',')
-     console.log(contact)
-     info = users.map(user => (contact[user] != undefined ? contact[obj].name+' '+contact[obj].lastName : (depart[user] != undefined ? depart[user].depart: '')),[])
+     info = users.map(user => (contact[user] != undefined ? contact[user].name+' '+contact[user].lastName : (depart[user] != undefined ? depart[user].depart: '')),[])
     }
     acc[it] = info
    } else {
