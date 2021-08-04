650525cf7612c88bc536f4e441353ac09b4c21f1
RighteouskilL <it@iris.co.th>
2021-08-04 17:21:35 +0700

    update

--- app/routers/announce.router.js
+++ app/routers/announce.router.js
@@ -10,6 +10,7 @@ module.exports = (app, router,upload) => {
   let file = req.file
   let body = req.body
   let path = file.path.match(/(\\public)(.*)/g)[0]
+  console.log()
   await con.q("INSERT INTO notice_data (note_title,note_desc,note_create,note_file) VALUES (?,?,?,?)",[body.note_title,body.note_desc,now,path])
   res.redirect('/')
  })
